00bc392b15fdd22d4b45db315cafeea66350f034
PARTH MAURYA <91662443+Parthm20@users.noreply.github.com>
2022-01-19 22:11:51 +0530

    Add files via upload

---
+++ half n by m.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n,m,i,;
+    scanf("%d%d",&n,&m);
+    for(i=1;i<=(m-1);i++)
+    {
+    while(n>0)
+    {
+        n=n/2;
+       
+    }
+    }
+    printf("%d",n);
+    return 0;
+}
+
