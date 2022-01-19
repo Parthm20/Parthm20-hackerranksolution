276aeedbe9e32e1a1d223ea9a65b9af0f6be050d
PARTH MAURYA <91662443+Parthm20@users.noreply.github.com>
2022-01-19 09:53:21 +0530

    Add files via upload

---
+++ array in ascending order.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,a[10],temp=0;
+    printf("enter the elements of array:\n");
+    for(i=0;i<10;i++)
+    {
+    scanf("%d",&a[i]);
+    }
+    for( i=0;i<10;i++)
+    {
+        for(j=i+1;j<10;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+            
+            
+        }
+    }
+    printf("elements in ascending order are :");
+    for(i=0;i<10;i++)
+    {
+    printf("%d\n",a[i]);
+    }
+}
